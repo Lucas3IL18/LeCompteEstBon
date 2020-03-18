20c6e7c8370c6b7994f0c647f305022aa0b18b87
Lucas <lucas@paumel.com>
2020-03-18 15:35:09 +0100

    Modif Interface
    Titre + logo

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,9 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.fxml.FXMLLoader;
 
 
@@ -12,9 +14,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Interface.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,700,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.setFill(Color.LIGHTGRAY);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Le Compte Est Bon");
+			primaryStage.getIcons().setAll(new Image(getClass().getResource("3ilogo.png").toExternalForm()));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
